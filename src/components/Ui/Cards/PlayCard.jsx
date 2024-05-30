fd1cf1cfedb110ac58dcedcad33b53ce79c07bd7
Yurii Danko <urilvv1@gmail.com>
2024-05-31 02:13:18 +0300

    implemented functionality to add other users playlists

--- src/components/Ui/Cards/PlayCard.jsx
+++ src/components/Ui/Cards/PlayCard.jsx
@@ -1,9 +1,10 @@
-import { Card, Image, Text } from '@mantine/core';
+import { ActionIcon, Card, Image, Text } from '@mantine/core';
 import React, { useEffect, useState } from 'react';
 import cl from './TrackLine.module.css';
 import axios from 'axios';
 import { useAuth } from '../../../hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import { IconPlus } from '@tabler/icons-react';
 
 const PlayCard = ({ playlist }) => {
   const [username, setUsername] = useState();
@@ -20,6 +21,21 @@ const PlayCard = ({ playlist }) => {
     });
 
     setUsername(userNameresponse.data.userName);
+  };
+
+  const handleClick = async (e) => {
+    try {
+      const response = await axios({
+        method: 'put',
+        url: `http://localhost:8080/add-playlist/${playlist.playlistId}?userId=${user.userId}`,
+        headers: {
+          Authorization: 'Bearer ' + user.token,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    window.location.reload();
   };
 
   const goToPlaylist = () => {
@@ -50,6 +66,11 @@ const PlayCard = ({ playlist }) => {
           {playlist.playlistName}
         </Text>
         <Text pb={5}>{username}</Text>
+        <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '10px' }}>
+          <ActionIcon radius={20} onClick={handleClick}>
+            <IconPlus></IconPlus>
+          </ActionIcon>
+        </div>
       </Card.Section>
     </Card>
   );
